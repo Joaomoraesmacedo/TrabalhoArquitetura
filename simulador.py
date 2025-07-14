14dc6c4d969b268e9dd71d47870f3358309fabf7
Eduarda Barragan <154289821+eduardabarragan@users.noreply.github.com>
2025-07-14 19:38:26 -0300

    Refatoracao do pipeline para o decodifica

--- simulador.py
+++ simulador.py
@@ -1,16 +1,14 @@
 import sys
 import io
-from enum import Enum, auto
+from enum  import Enum, auto
 from dataclasses import dataclass
 from typing import Optional
+
 
 class Comando(Enum):
     MOVI = auto()
     ADD = auto()
     ADDI = auto()
-    LOAD = auto()
-    STORE = auto()
-
     #colocar outras coisas dps
 
 @dataclass
@@ -35,107 +33,51 @@ def inicializa_registradores() -> list:
         lista_valores.append(0)
     return lista_registradores, lista_valores  
 
-def busca_inst(pipeline: list, linhas: list[str], pc: int) -> int:
-    if pc < len(linhas):
-        pipeline[0] = linhas[pc].strip()
-        pc += 1
-    else:
-        pipeline[0] = None
-    return pc
-
-def decodifica(pipeline: list, linhas: list[str]):
-    if pipeline[1] is None and pipeline[0] is not None:
-        text = pipeline[0]
-        linha = text.split(" ")
+def lista_instrucoes(linhas:list[str]) -> list[Instrucao]:
+    lista: list[Instrucao] = []
+    n = 0 #indice da isntrução
+    
+    for op in linhas:
+        text = op.strip()
+        linha = op.split(" ")
         instrucao = linha[0].upper()
-        resto = linha[1]
-
+        resto =linha[1]
+        
         if instrucao == Comando.MOVI.name:
-            reg, imm_str = resto.split(",")
+            reg, imm_str  = resto.split(",")
             rd = int(reg[1:])
-            instr = Instrucao(Comando.MOVI, rd, None, None, int(imm_str), text)
+            rs = None
+            rt = None
+            imm = int(imm_str)
+            print(f'{n} rd:"{rd}" rs:"{rs}" rt:"{rt}" imm:"{imm}" opcode:"{instrucao.lower()}" text:"{text}"')
+            executa = Instrucao(Comando.MOVI, rd, rs, rt, imm, text)
+            lista.append(executa)
+           
+           
         elif instrucao == Comando.ADD.name:
-            regs = resto.split(",")
+            regs  = resto.split(",")
             rd = int(regs[0][1:])
             rs = int(regs[1][1:])
             rt = int(regs[2][1:])
-            instr = Instrucao(Comando.ADD, rd, rs, rt, None, text)
+            imm = None
+            print(f'{n} rd:"{rd}" rs:"{rs}" rt:"{rt}" imm:"{imm}" opcode:"{instrucao.lower()}" text:"{text}"')
+            executa = Instrucao(Comando.ADD, rd, rs, rt, imm, text)
+            lista.append(executa)
+           
         elif instrucao == Comando.ADDI.name:
-            regs = resto.split(",")
+            regs  = resto.split(",")
             rd = int(regs[0][1:])
             rs = int(regs[1][1:])
-            instr = Instrucao(Comando.ADDI, rd, rs, None, int(regs[2]), text)
-        else:
-            print("Instrução inválida:", text)
-            instr = None
-
-        pipeline[1] = instr
-        pipeline[0] = None
-
-def verica_hazard(pipeline: list) -> int:
-    if pipeline[1] is not None:
-        for i in range(3, 1, -1):
-            if pipeline[i] is not None:
-                if pipeline[1].rs == pipeline[i].rd or pipeline[1].rt == pipeline[i].rd:
-                    return pipeline[i].rd
-    return None
-
-def executa_inst(pipeline: list, valor_reg: list[int], resultados: list[Resultado]) -> None:
-    if isinstance(pipeline[2], Instrucao):
-        instrucao: Instrucao = pipeline[2]
-        rd = instrucao.rd
-        rs = instrucao.rs
-        rt = instrucao.rt
-        imm = instrucao.imm
-
-        if instrucao.inst == Comando.MOVI:
-            valor = imm
-        elif instrucao.inst == Comando.ADD:
-            valor = valor_reg[rs] + valor_reg[rt]
-        elif instrucao.inst == Comando.ADDI:
-            valor = valor_reg[rs] + imm
-        else:
-            valor = 0
-
-        resultados.append(Resultado(rd, valor))
-
-def escreve_reg(pipeline: list, valor_reg: list[int], resultados: list[Resultado]) -> None:
-    if pipeline[4] is not None and resultados:
-        posi = resultados[0].reg
-        valor_reg[posi] = resultados[0].valor
-        resultados.pop(0)
-
-def finaliza(pipeline: list, linhas: list[str], pc: int) -> bool:
-    comando = True
-    verifica_noop = True 
-    for i in pipeline:
-        if i != None:
-            verifica_noop = False
-    if verifica_noop and pc>= len(linhas):
-        comando = False
-    return comando
-
-
-def executa_operacoes(operacao: io.TextIOWrapper, nome_reg: list[str], valor_reg: list[int], memoria: list[int]):
-    linhas = operacao.readlines()
-    pipeline: list = [None] * 5
-    resultados = []
-    pc = 0
-    comando = True
-    tam = len(pipeline) - 1
-
-    str_pipeline = "|-----Busca-----||---Decodifica--||---Executa-----||---Memoria-----||----Regist-----|"
-
-    while comando:
-        hazard = verica_hazard(pipeline)
-        if hazard is not None:
-            for i in range(4, 2, -1):
-                pipeline[i] = pipeline[i - 1]
-            pipeline[2] = None
+            rt = None
+            imm = int(regs[2])
+            print(f'{n} rd:"{rd}" rs:"{rs}" rt:"{rt}" imm:"{imm}" opcode:"{instrucao.lower()}" text:"{text}"')
+            executa = Instrucao(Comando.ADDI, rd, rs, rt, imm, text)
+            lista.append(executa)
+        
+           
         else:
             print('Esta instrução não existe')
         
-        print(lista)
             
         n += 1
     return lista
@@ -162,46 +104,116 @@ def indentifica_noop(lista:list[str], op: int):
         return lista[op].strip()
     else:
         return "NOOP"
-        
-def executa_operacoes(operacao:io.TextIOWrapper, nome_reg:list[str], valor_reg:list[int], memoria:list[int]):
 
-    linhas = operacao.readlines()
-    escreve_reg = len(linhas) # programa termina quando escrever todas os resultados instruções nos registradores
-    lista_inst = lista_instrucoes(linhas)
-    pipeline = [None,None,None,None,None] #pipeline com as operacoes 
-    pc = 0 #inicia o pc 
-    str_pipeline = "|-----Busca-----||---Decodifica--||---Executa-----||---Memoria-----||----Regist-----|" #15 caracteres dentro de cada /
-    i = 0
-    comando = True
-    while comando:
-        for i in range(4, 0, -1): # avanac as instrucoes do pipeline a cada ciclo 
-            pipeline[i] = pipeline[i-1]
+def busca_inst(pipeline: list, lista_inst: list[Instrucao], pc: int) -> int:
+    if pc < len(lista_inst):
+        pipeline[0] = lista_inst[pc]
+        pc +=1 
+    else:
+        pipeline[0] = None
+    return pc
 
-        if pc < len(lista_inst):  #busca da proxima instrucao 
-            pipeline[0] = lista_inst[pc] 
-            pc +=1 
-        else:
-            pipeline[0] = None
+def verica_hazard(pipeline: list[Instrucao]) -> int:
+    '''Retorna int do hazard que esta mais proximo da instrução, ou seja, aquele que irá demorar mais para ser resolvido'''
+    if pipeline[1] is not None:
+        for i in range(3,1,-1):
+            if pipeline[i] is not None:
+                if pipeline[1].rs == pipeline[i].rd or pipeline[1].rt == pipeline[i].rd:
+                    return  pipeline[i].rd
+    return None
 
-        instrucao= pipeline [2]
-        if instrucao is not None: #faz a execucao de instrucoes 
+def executa_inst(pipeline: list[Instrucao], valor_reg: list[int], resultados: list[Resultado]) -> list[Resultado]:
+    if pipeline[2] is not None: #faz a execucao de instrucoes 
+            instrucao: Instrucao = pipeline[2]
             rd = instrucao.rd  
             rs = instrucao.rs
             rt = instrucao.rt 
             imm = instrucao.imm
-        if instrucao is not None:
+
+            #Dar um  jeito de armazenar esse valor e usar dps que o registro saiu desse estado
             if instrucao.inst == Comando.MOVI:      
-                valor_reg[rd] = imm
+               valor = imm
             elif instrucao.inst == Comando.ADD:
-                valor_reg[rd] = valor_reg[rs] + valor_reg[rt]
+                valor = valor_reg[rs] + valor_reg[rt]
             elif instrucao.inst == Comando.ADDI:
-                valor_reg[rd] = valor_reg[rs] + imm
+                valor = valor_reg[rs] + imm
+
+            result = Resultado(rd, valor)
+            resultados.append(result)
+
+def escreve_reg(pipeline: list[Instrucao], valor_reg: list[int], resultados: list[Resultado]):
+    if pipeline[4] is not None:
+        posi = resultados[0].reg
+        valor_reg[posi] = resultados[0].valor
+        resultados.pop(0)
+
+
+def finaliza(pipeline, lista_inst, pc) -> bool:
+    comando = True
+    verifica_noop = True 
+    for i in pipeline:
+        if i != None:
+            verifica_noop = False
+    if verifica_noop and pc>= len(lista_inst):
+        comando = False
+    return comando
+
+def executa_operacoes(operacao:io.TextIOWrapper, nome_reg:list[str], valor_reg:list[int], memoria:list[int]):
+
+    linhas = operacao.readlines()
+    lista_inst = lista_instrucoes(linhas)
+    pipeline: list[Instrucao] = [None,None,None,None,None] #pipeline com as operacoes 
+    tam = len(pipeline) - 1
+    pc = 0 #inicia o pc 
+    resultados = []
+    str_pipeline = "|-----Busca-----||---Decodifica--||---Executa-----||---Memoria-----||----Regist-----|" #15 caracteres dentro de cada /
+    comando = True
+   
+    while comando:
         
-        if all(stage is None for stage in pipeline) and pc >= len(lista_inst): #tive qude ver no chat essa parte do all, mas ele
-            comando = False                                                     # verifica se todos os estagios do pipeline ta vazio 
+        hazard = verica_hazard(pipeline) 
+        if hazard is not None:
+            for i in range(4,2,-1):
+                pipeline[i] = pipeline[i - 1]
+            pipeline[2] = None
+
+
+        else:
+            for i in range(tam, 0, -1):
+                pipeline[i] = pipeline[i - 1]
+
+            # Busca a próxima instrução
+            pc = busca_inst(pipeline, lista_inst, pc)
+
+       
 
         
-    while escreve_reg > 0:
+        #Verifica se todos os estagios do pipeline ta vazio 
+        executa_inst(pipeline, valor_reg, resultados)
+     
+
+        #Print
+        saida = "  "
+        for i in pipeline:
+            if i is not None:
+                saida += i.text +"  "
+            else:
+                saida += "NOOP   " 
+
+        print(str_pipeline)
+        print(saida)
+        nomes = "  "
+        for i in nome_reg:
+            nomes += i + "  "
+        print(nomes)
+        print(valor_reg)
+        print(pc)
+
+        escreve_reg(pipeline, valor_reg, resultados)
+      
+
+        comando = finaliza(pipeline, lista_inst, pc)
+    ''''while escreve_reg > 0:
             print(str_pipeline)
             print(f"|{calcula_caracteres(indentifica_noop(linhas, i))}||{calcula_caracteres(indentifica_noop(linhas, i - 1))}||{calcula_caracteres(indentifica_noop(linhas, i-2))}||{calcula_caracteres(indentifica_noop(linhas, i - 3))}||{calcula_caracteres(indentifica_noop(linhas, i - 4 ))}|") # pipeline 
             print(" ")
@@ -221,25 +233,29 @@ def executa_operacoes(operacao:io.TextIOWrapper, nome_reg:list[str], valor_reg:l
             print(f"escreve_reg restante: {escreve_reg}")
 
             
-        
-        #Escrever todo o codigo aqui primeiro BI, BO EX, EI (escreve_reg incrementa aqui) depois implementar hazards 
-        #Somente utilizando a lista_inst a instrução 0 está no indice 0 e assim por diante 
         #Para saber o que esta na lista execute no terminal: python3 simulador.py add_mov.txt
+        '''
         
-   
 
 
 def main() -> None:
     if len(sys.argv) == 3:
-        nomeArq = sys.argv[1]
+        nomeArq = sys.argv[1] 
         tam_memoria = int(sys.argv[2])
         operacao = open(nomeArq, 'r')
-        nome_reg, valor_reg = inicializa_registradores()
-        memoria = [0] * tam_memoria
+        nome_reg, valor_reg = inicializa_registradores() #registradores = 0
+        memoria = [] #VERIFICAR COMO QUE INICIALIZA A MEMORIA 
+        for i in range(tam_memoria):
+            memoria.append(0)
+        
+        print(memoria)
 
         executa_operacoes(operacao, nome_reg, valor_reg, memoria)
+    
     else:
         print("Faltou comandos na linha de parâmetro, deve conter: Script, Arquivo_Operações e um inteiro indicando o tamanho da memória")
+
+
 
 if __name__ == "__main__":
     main()
